d30e94ead09465f06c2297504677dc339e08abcd
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2020-04-28 23:54:18 +0300

    create UnavailableAlert component

---
+++ src/components/UnavailableAlert.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-alert show dismissible variant="danger">
+    Not available statistics for this country!
+  </b-alert>
+</template>
+
+<script>
+export default {
+  name: "UnavailableAlert"
+};
+</script>
+
+<style></style>

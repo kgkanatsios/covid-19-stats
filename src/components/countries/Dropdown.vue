8171b1c1bdda4c6534be2ab7726e26a850e776cd
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2020-04-28 23:57:11 +0300

    update url on country click/select

--- src/components/countries/Dropdown.vue
+++ src/components/countries/Dropdown.vue
@@ -7,14 +7,21 @@
     class="m-2"
     menu-class="w-100 countries-list overflow-auto"
   >
-    <b-dropdown-item @click="setCurrentCountry(['Global', null])"
+    <b-dropdown-item
+      @click="
+        setCurrentCountry(['Global', null]);
+        updateBrowserUrl(['Global', null]);
+      "
       >Global</b-dropdown-item
     >
     <b-dropdown-divider></b-dropdown-divider>
     <b-dropdown-item
       v-for="(country, index) in countryList"
       :key="index"
-      @click="setCurrentCountry([country.name, country.alpha3Code])"
+      @click="
+        setCurrentCountry([country.name, country.alpha3Code]);
+        updateBrowserUrl([country.name, country.alpha3Code]);
+      "
     >
       <b-avatar variant="dark" :src="country.flag" class="mr-2"></b-avatar>
       {{ country.name }}
@@ -32,7 +39,22 @@ export default {
   methods: {
     ...mapActions({
       setCurrentCountry: "setCurrentCountry"
-    })
+    }),
+    updateBrowserUrl([name, alpha3Code]) {
+      this.$router.push(
+        name == "Global"
+          ? {
+              name: "Home"
+            }
+          : {
+              name: "Country",
+              params: {
+                alpha3Code: alpha3Code,
+                name: name
+              }
+            }
+      );
+    }
   },
   computed: {
     ...mapGetters(["countryList"])

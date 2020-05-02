01e0ec7ccfa8db2b78eff3cb351eadd317eeb352
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2020-05-02 18:17:10 +0300

    modify header for mobile/tablet view

--- src/App.vue
+++ src/App.vue
@@ -2,29 +2,32 @@
   <div id="app">
     <b-navbar toggleable="lg" type="dark" variant="danger" class="mb-4">
       <b-container>
-        <b-navbar-brand to="/" class="mr-3 d-flex align-content-center"
+        <b-navbar-brand to="/" class="mr-3 d-flex align-content-center order-0"
           >CO<v-icon
             name="brands/vuejs"
             class="my-auto"
             scale="1.25"
           />ID19</b-navbar-brand
         >
-        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-        <b-collapse id="nav-collapse" is-nav>
-          <b-navbar-nav class="mx-auto">
-            <li class="nav-item">
-              <b-link to="/" class="nav-link text-white">Home</b-link>
-            </li>
-            <li class="nav-item">
-              <b-link to="/map" class="nav-link text-white">Map</b-link>
-            </li>
-            <li class="nav-item">
-              <b-link to="/about" class="nav-link text-white">About</b-link>
-            </li>
-          </b-navbar-nav>
-        </b-collapse>
+        <div class="mx-auto order-2 order-lg-1">
+          <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+          <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav class="mx-auto">
+              <li class="nav-item">
+                <b-link to="/" class="nav-link text-white">Home</b-link>
+              </li>
+              <li class="nav-item">
+                <b-link to="/map" class="nav-link text-white">Map</b-link>
+              </li>
+              <li class="nav-item">
+                <b-link to="/about" class="nav-link text-white">About</b-link>
+              </li>
+            </b-navbar-nav>
+          </b-collapse>
+        </div>
+
         <b-navbar-brand
-          class="mr-0 ml-3 d-flex align-content-center"
+          class="mr-0 ml-3 d-flex align-content-center order-1 order-lg-1"
           href="https://github.com/kgkanatsios/covid-19-stats"
           target="_blank"
           >GitHub <v-icon name="brands/github" class="my-auto ml-1" scale="1.25"

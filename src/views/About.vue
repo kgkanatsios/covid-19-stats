86af3c17c3dec1428172de9f4474329bf7906404
Konstantinos Gkanatsios (kgkanatsios) <kgkanatsios@gmail.com>
2020-05-02 18:16:18 +0300

    modify content of About.vue

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container>
+    <b-row>
+      <b-col class="text-left">
+        <h1 class="border-bottom border-dark mb-3 pb-1">Covid-19-Stats</h1>
+        <p>
+          Covid-19-Stats is single-page application (SPA) that shows the latest
+          COVID-19 statistics feed (infected, recovered and deaths caused by
+          Coronavirus) of the world and your country of choice in real-time,
+          based on data from
+          <a href="https://covidapi.info/" target="_blank">Covid19 API</a>.
+        </p>
+
+        <h2 class="border-bottom border-dark mb-3 pb-1">Data Source</h2>
+        <ul>
+          <li>
+            <a
+              href="https://documenter.getpostman.com/view/2568274/SzS8rjbe?version=latest"
+              target="_blank"
+              >Covid19 API</a
+            >
+          </li>
+          <li>
+            <a href="https://restcountries.eu/" target="_blank"
+              >REST COUNTRIES</a
+            >
+          </li>
+        </ul>
+        <h2 class="border-bottom border-dark mb-3 pb-1">
+          Technology used in this project
+        </h2>
+        <ul>
+          <li>Vue.js</li>
+          <li>Vuex</li>
+          <li>Vue-router</li>
+          <li>Axios</li>
+          <li>Vue-ChartJS</li>
+          <li>Vue2-Leaflet</li>
+          <li>Bootstrap-Vue</li>
+          <li>Vue-Awesome</li>
+        </ul>
+        <h2 class="border-bottom border-dark mb-3 pb-1">Disclaimer</h2>
+        <p>
+          WE DO NOT GUARANTEE ACCURACY OF THE DATA. PLEASE USE IT AS A VISUAL
+          AID ONLY, AND DO NOT BASE ANY DECISION MEDICAL OR LOGISTICAL BASED ON
+          THE DATA PROVIDED IN THESE CHARTS OR DATA FILES.<br /><br />
+          Please note that statistics collection, especially in early days of
+          pandemic was erratic, patchy, and not systemized.
+        </p>
+      </b-col>
+    </b-row>
+  </b-container>
 </template>
